3f97b0d1e53d2ca95fa7d8506fa2cb0d6ac08c2d
Rogerio dos Santos <rogerio.santos.main@gmail.com>
2015-09-04 07:06:43 -0500

    Removed auto layout and added xeditable

--- leel_third_party.js
+++ leel_third_party.js
@@ -1,4 +1,5 @@
-/// <reference path="./typings/tsd.d.ts" />
+/// <reference path='./typings/tsd.d.ts' />
 
-module.exports.T = require("./third_party/template/t.js");
+module.exports.T = require('./third_party/template/t.js');
+module.exports.xEditable = require('./third_party/angular-xeditable-0.1.8/js/xeditable.js');
 
