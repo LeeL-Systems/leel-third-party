8167a0bb93038380e37faa84d80ecd56afeb345f
Rogerio dos Santos <rogerio.santos.main@gmail.com>
2016-05-08 07:54:13 -0400

    Insert crosstool-ng

---
+++ build/prepare_crosstool-ng.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+clear
+
+# Location and Definitions
+# - - - - - - - - - - - - - - - -
+
+cd "$(dirname "$0")"
+current_dir=$(pwd)
+
+echo ""
+echo "### Configuring crosstool-ng utility ###"
+
+echo ""
+echo "--- Installing required components ---"
+
+sudo apt-get install bison cvs flex gperf texinfo automake libtool help2man gawk libtool-bin libncurses5-dev
+
+echo ""
+echo "--- Installing crosstool-ng ---"
+
+cd ../crosstool-ng/installer
+
+bin_dir="${HOME}/bin"
+echo "User bin Dir: ${bin_dir}"
+
+installation_dir="${bin_dir}/crosstool-ng"
+echo "Installation Dir: ${installation_dir}"
+mkdir -p "${installation_dir}"
+
+
+./configure -prefix=${installation_dir}
+make
+make install
+
+ln -s "${installation_dir}/bin/ct-ng" "${bin_dir}/ct-ng"
+
+export PATH="${PATH}:${installation_dir}/bin"
+
+echo ""
+echo "--- Installation Summary ---"
+echo "Installation Dir: ${installation_dir}"
+
+
---
+++ crosstool-ng/README.txt
@@ -0,0 +1,5 @@
+Crosstool-ng is an utility that allow us to build Toolchains.
+
+Download Website: http://crosstool-ng.org/download/crosstool-ng/?sort=modtime&order=desc
+
+Version Installed: 1.22.0
