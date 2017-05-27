6ba917a1df8ee2a95aeeabda3a52e5aab13e4c15
Roger Santos <roger.santos.main@gmail.com>
2017-05-27 13:07:39 -0500

    Added build lib projects

---
+++ build/libs/boost/program_options/build/compile_linux_default.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+echo "### Compile boost_program_options ###"
+
+# Build configuration
+config_project_name="boost_program_options"
+config_platform="linux_default"
+config_flavor="release"
+
+# Exit on any non-zero status.
+trap 'exit' ERR
+set -E
+
+echo " "
+echo "*** Build Configuration:" 
+echo "1 - Project Name: $config_project_name"
+echo "2 - Platform: $config_platform"
+echo "3 - Flavor: $config_flavor"
+
+# Get variables based on the configuration
+cd "$(dirname "$0")"
+bash_dir=$(pwd)
+
+build_dir="$bash_dir/$config_project_name"
+mkdir -p $build_dir
+cd $build_dir
+build_dir="$(pwd)/$config_platform"
+mkdir -p $build_dir
+cd $build_dir
+build_dir="$(pwd)/$config_flavor"
+mkdir -p $build_dir
+cd $build_dir
+echo "A - Build Directory: $build_dir"
+
+echo " "
+echo "*** Staging:" 
+cmake ../../../.. 
+
+echo " "
+echo "*** Compiling:" 
+# make VERBOSE=1
+# make
+cmake --build . --config Release --clean-first
+
